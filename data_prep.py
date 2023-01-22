c09260d26eee8c0b086de29fe333a18579ef71af
nowayqq <set.mr@inbox.ru>
2023-01-22 21:52:01 +1000

    little fix

--- data_prep.py
+++ data_prep.py
@@ -9,10 +9,10 @@ def get_data(path):
     with codecs.open(path, encoding='utf-8') as fin:
         lines = fin.readlines()
 
-    if 'Title:' in lines[0]:
+    if 'Title: ' in lines[0]:
         title = lines[0].replace('\r', '').replace('\n', '')
         lines.pop(0)
-        title.replace('Title: ', '')
+        title = title.replace('Title: ', '')
 
     for line in lines:
         text += line.replace('\r', '').replace('\n', '')
